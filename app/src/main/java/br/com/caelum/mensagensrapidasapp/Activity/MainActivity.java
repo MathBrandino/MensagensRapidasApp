c2a780fa5d27c7861b94a38d25e60370b9575472
Matheus Brandino <m.v.brandino@gmail.com>
2015-07-13 17:46:43 -0300

    consumindo click

--- app/src/main/java/br/com/caelum/mensagensrapidasapp/Activity/MainActivity.java
+++ app/src/main/java/br/com/caelum/mensagensrapidasapp/Activity/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
                         .show();
 
 
-                return false;
+                return true;
             }
         });
 
